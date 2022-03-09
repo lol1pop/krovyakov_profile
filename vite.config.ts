8439c5815e83ea66522b6cef85f574946b6e0f48
Denis Krovyakov <cpl.fassbender@gmail.com>
2022-03-09 19:49:16 +0700

    edit public dir

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/krovyakov_profile/',
+  publicDir: 'assets/static',
   plugins: [vue()],
   resolve: {
     alias: {
