7b02073a6aca50fccbf7e9a56cab98c7c8469278
Denis Krovyakov <cpl.fassbender@gmail.com>
2022-03-09 19:43:10 +0700

    add Main Pages

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <script setup lang="ts">
+import MainPage from '@/views/MainPage.vue';
 </script>
 
 <template>
-  <header>
-  </header>
-
-  <main>
-  </main>
+  <div class="layout">
+    <main class="container">
+      <header>
+      </header>
+      <MainPage/>
+    </main>
+  </div>
 </template>
 
-<style>
+<style lang="scss">
+@import 'assets/css/colors';
+@import "assets/css/mixin";
+main {
+  background-color: $dark;
+  @include box-shadow(0px 0px 30px 0px $D-dark-purple);
+  min-height: 1360px; /*TODO: replace 100%*/
+  padding: 0;
+}
+
+.layout {
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  height: 1800px; /*TODO: replace 100%*/
+  background-color: $dark-background;
+}
 </style>
